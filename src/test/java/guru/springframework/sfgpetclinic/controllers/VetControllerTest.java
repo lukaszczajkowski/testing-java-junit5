709b3876b45e2242c2cf8c70b3bb0a46126cbb02
lukaszczajkowski <lukaszczajkowski.wroc@gmail.com>
2022-02-27 20:20:55 +0100

    First test assignment completed

---
+++ src/test/java/guru/springframework/sfgpetclinic/controllers/VetControllerTest.java
@@ -0,0 +1,46 @@
+package guru.springframework.sfgpetclinic.controllers;
+
+import guru.springframework.sfgpetclinic.fauxspring.Model;
+import guru.springframework.sfgpetclinic.fauxspring.ModelImpl;
+import guru.springframework.sfgpetclinic.fauxspring.ModelMap;
+import guru.springframework.sfgpetclinic.model.Speciality;
+import guru.springframework.sfgpetclinic.model.Vet;
+import guru.springframework.sfgpetclinic.services.VetService;
+import guru.springframework.sfgpetclinic.services.map.SpecialityMapService;
+import guru.springframework.sfgpetclinic.services.map.VetMapService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.*;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class VetControllerTest {
+
+    VetController vetController;
+    VetService vetService;
+    SpecialityMapService specialityMapService;
+    Model model;
+
+    @BeforeEach
+    void setUp() {
+        specialityMapService = new SpecialityMapService();
+        vetService = new VetMapService(specialityMapService);
+        vetController = new VetController(vetService);
+
+        model = new ModelImpl();
+
+        vetService.save(new Vet(1L, "Anna", "Malecka", new HashSet<>(Set.of(new Speciality("dogs")))));
+        vetService.save(new Vet(2L, "Lukasz", "Czajkowski", new HashSet<>(Set.of(new Speciality("possums")))));
+    }
+
+    @Test
+    void shouldListVets() {
+        String view = vetController.listVets(model);
+
+        assertThat(view).isEqualTo("vets/index");
+
+        Set<Vet> modelAttribute = (Set<Vet>) ((ModelImpl) model).getMap().get("vets");
+        assertThat(modelAttribute.size()).isEqualTo(2);
+    }
+}
