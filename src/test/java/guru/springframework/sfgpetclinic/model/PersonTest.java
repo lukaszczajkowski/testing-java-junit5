531ce9ab1cd6bf9e698bd7a9e7866138ce567cac
lukaszczajkowski <lukaszczajkowski.wroc@gmail.com>
2022-03-01 11:59:11 +0100

    JUnit test dependency injections

--- src/test/java/guru/springframework/sfgpetclinic/model/PersonTest.java
+++ src/test/java/guru/springframework/sfgpetclinic/model/PersonTest.java
@@ -1,9 +1,7 @@
 package guru.springframework.sfgpetclinic.model;
 
 import guru.springframework.sfgpetclinic.ModelTests;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.RepeatedTest;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -30,10 +28,14 @@ class PersonTest implements ModelTests {
                 () -> assertEquals("Buck", person.getLastName(), "Last Name Failed"));
     }
 
-    @Test
     @RepeatedTest(value = 10, name = "{displayName}: {currentRepetition} - {totalRepetitions}")
     @DisplayName("My repeated test")
     void myRepeatedTest() {
         //todo: impl
+    }
+
+    @RepeatedTest(5)
+    void myRepeatedTestWithDi(TestInfo testInfo, RepetitionInfo repetitionInfo) {
+        System.out.println(testInfo.getDisplayName() + ": " + repetitionInfo.getCurrentRepetition());
     }
 }
